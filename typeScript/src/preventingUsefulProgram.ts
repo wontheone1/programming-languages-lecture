9d9ac1127eb96bddfe52116982a8ef18e373f6b4
Won Seob Seo <pghoou@gmail.com>
2020-11-18 01:44:28 +0200

    Show typescript demo

--- typeScript/src/preventingUsefulProgram.ts
+++ typeScript/src/preventingUsefulProgram.ts
@@ -1,1 +1,11 @@
-console.log(1, 8, 2);
+// if we don't allow any type ("noImplicitAny": true), annotating allowed type make this work.
+let myFunc = (f: (arg: number | boolean) => (number | boolean)[]) => [
+  f(7),
+  f(true),
+];
+
+let makePair = (x: number | boolean) => [x, x];
+
+console.log([makePair(7), makePair(true)]);
+
+console.log(myFunc(makePair));
