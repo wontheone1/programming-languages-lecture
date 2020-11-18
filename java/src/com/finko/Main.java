1e574daad7247ea278a190ef305e8d4ceb3ea304
Won Seob Seo <pghoou@gmail.com>
2020-11-18 08:08:27 +0200

    Show getDouble function

--- java/src/com/finko/Main.java
+++ java/src/com/finko/Main.java
@@ -24,11 +24,23 @@ public class Main {
 //    }
 
     // array-bounds error
+//    public static void main(String[] args) {
+//        System.out.println("I will be printed because this program compiles!");
+//        int[] myArray = {1, 2, 3, 4, 5};
+//        System.out.println(myArray[0]);
+//        System.out.println(myArray[5]); // out of bound
+//    }
+
+    // Cannot use getDouble function for String or int. Seems like there is no easy way in Java.
+    // Just use two different getDoubleFunction for int or String
+    // (or maybe you can define a class that holds a number and string?)
     public static void main(String[] args) {
-        System.out.println("I will be printed because this program compiles!");
-        int[] myArray = {1, 2, 3, 4, 5};
-        System.out.println(myArray[0]);
-        System.out.println(myArray[5]); // out of bound
+        System.out.println(getDouble(5));
+        System.out.println(getDouble(10));
+    }
+
+    private static int getDouble(int arg) {
+        return arg+arg;
     }
 
 }
