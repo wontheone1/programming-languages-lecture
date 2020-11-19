04af2ab98204ffb4cbf9ce9463a248e2cd0e8fd1
Won Seob Seo <pghoou@gmail.com>
2020-11-19 22:19:13 +0200

    Use double for more types

--- js/returnDifferentTypes.js
+++ js/returnDifferentTypes.js
@@ -7,3 +7,19 @@ const double = (arg) => arg + arg;
 
 console.log(double(5));
 console.log(double("doubled"));
+
+// const double = (arg) => {
+//   if (typeof arg === "string" || typeof arg === "number") {
+//     return arg + arg;
+//   }
+//   if (Array.isArray(arg)) {
+//     return [...arg, ...arg];
+//   } else {
+//     return [arg, arg];
+//   }
+// };
+
+// console.log(double(5));
+// console.log(double("doubled"));
+// console.log(double([1, 2, 3]));
+// console.log(double({ a: 33 }));
